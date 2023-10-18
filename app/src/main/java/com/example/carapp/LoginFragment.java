f386ac50d523c569214481688ca0073c4cb89e62
Nicholas Trambitas <ntrambitas12@gmail.com>
2023-10-17 21:06:40 -0400

    fixed bug with app crashing in night mode

--- app/src/main/java/com/example/carapp/LoginFragment.java
+++ app/src/main/java/com/example/carapp/LoginFragment.java
@@ -2,7 +2,9 @@ package com.example.carapp;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -40,8 +42,9 @@ public class LoginFragment extends Fragment {
         super.onCreate(savedInstanceState);
         mAuth = FirebaseAuth.getInstance();
         // if any arguments are passed, pull them out here
-        System.out.println("Created fragment");
+        //System.out.println("Created fragment");
     }
+
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
