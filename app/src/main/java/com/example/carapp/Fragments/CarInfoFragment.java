b63b3a19609bdb450a7d2ce266703663ef34962c
dylansussman <dylansussman17@gmail.com>
2023-10-30 23:29:42 -0400

    Simple display/edit view for car info

--- app/src/main/java/com/example/carapp/Fragments/CarInfoFragment.java
+++ app/src/main/java/com/example/carapp/Fragments/CarInfoFragment.java
@@ -5,7 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -16,66 +16,63 @@ import androidx.navigation.Navigation;
 
 import com.example.carapp.R;
 import com.example.carapp.ViewModels.FirebaseManager;
-import com.google.firebase.auth.FirebaseAuth;
 
-public class CarDashboardFragment extends Fragment {
+import java.util.HashMap;
+
+public class CarInfoFragment extends Fragment {
 
     private NavController navController;
-    private FirebaseAuth mAuth;
     private FirebaseManager firebaseManager;
-    private String carName;
-    private String carMakeModel;
-    private String carVIN;
+    private HashMap<String, Object> car;
+    private EditText carNickname;
+    private EditText carMacAddress;
+    private EditText carVIN;
+    private EditText carColor;
+    private int vinNumber;
 
-    public CarDashboardFragment(String carName, String carMakeModel, String carVIN)
-    {
-        this.carName = carName;
-        this.carMakeModel = carMakeModel;
-        this.carVIN = carVIN;
+    public CarInfoFragment(int vinNumber) {
+        this.vinNumber = vinNumber;
     }
 
-    public CarDashboardFragment()
-    {
-        this.carName = "Car Name";
-        this.carMakeModel = "Car Make And Model";
-        this.carVIN = "VIN";
-    }
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mAuth = FirebaseAuth.getInstance();
         firebaseManager = new ViewModelProvider(requireActivity()).get(FirebaseManager.class);
     }
 
-    @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.dashboard_with_car, container, false);
-        TextView carNameText = rootView.findViewById(R.id.CarNameText);
-        TextView carMakeText = rootView.findViewById(R.id.CarMakeModel);
-        carNameText.setText(this.carName);
-        carMakeText.setText(this.carMakeModel);
-
-        Button removeCar = rootView.findViewById(R.id.RemoveCar);
-        removeCar.setOnClickListener(this.createListener());
-
+        View rootView = inflater.inflate(R.layout.view_car_info, container, false);
         return rootView;
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
-        // Get navigation controller
         navController = Navigation.findNavController(requireActivity(), R.id.Nav_Dashboard);
+
+        Button save = view.findViewById(R.id.saveCarInfo);
+        Button cancel = view.findViewById(R.id.cancelCarInfo);
+        carNickname = view.findViewById(R.id.editCarNickname);
+        carMacAddress = view.findViewById(R.id.editCarMacAddress);
+        carVIN = view.findViewById(R.id.editCarVIN);
+        carColor = view.findViewById(R.id.editCarColor);
+
+        // TODO Need to pass args thru navigation to know which car to display
+
+        save.setOnClickListener(this.createListener());
+        cancel.setOnClickListener(this.createListener());
+
     }
 
     private View.OnClickListener createListener() {
         return new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (view.getId() == R.id.RemoveCar) {
-                    firebaseManager.deleteCar(mAuth.getUid(), carVIN);
+                if (view.getId() == R.id.saveCarInfo) {
+
+                } else if (view.getId() == R.id.cancelCarInfo) {
+
                 }
             }
         };
